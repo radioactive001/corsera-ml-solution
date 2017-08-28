b5d7d105ff99ab44eac292dc00fadc145fb3b567
Anik Pramanik <anikpramanik.bd@gmail.com>
2017-08-29 01:25:14 +0530

    Solution upto ex3

---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,52 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+ x1=X(:,1); 
+ x2=X(:,2); 
+ s1=(alpha*(sum(((X*theta)-y)'*x1)))/m; 
+ s2=(alpha*(sum(((X*theta)-y)'*x2)))/m;
+ 
+ thetate1=theta(1)-s1;
+ thetate2=theta(2)-s2;
+ 
+ theta=[thetate1;thetate2];
+ disp(sum(((X*theta)-y)'*x1));
+ 
+
+ 
+
+
+ 
+ 
+ 
+
+    
+
+
+
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
---
+++ machine-learning-ex2/ex2/predict.m
@@ -0,0 +1,33 @@
+function p = predict(theta, X)
+%PREDICT Predict whether the label is 0 or 1 using learned logistic 
+%regression parameters theta
+%   p = PREDICT(theta, X) computes the predictions for X using a 
+%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+
+m = size(X, 1); % Number of training examples
+
+% You need to return the following variables correctly
+p = zeros(m, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters. 
+%               You should set p to a vector of 0's and 1's
+%
+
+
+A=sigmoid(X*theta);
+
+for i=1:size(A,1)
+
+if(A(i)>=.5)
+    A(i)=1;
+elseif (A(i)<.5)
+    A(i)=0;
+end
+end
+p=A;
+% =========================================================================
+
+
+end
